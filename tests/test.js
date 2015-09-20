40d50aced1b09bef74a01e0b8e445fa177fbef2c
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-09-20 09:03:59 -0400

    Update tests to maybe run from node

--- tests/test.js
+++ tests/test.js
@@ -1,19 +1,42 @@
-var expect = chai.expect;
+var require = require || null;
 
-describe('requirator', function() {
+if (require) {
+  /*
+   * running in node;
+   * load dependencies
+   */
+   var chai = require('chai');
+   require('../lib/requirator.js');
+  test();
+} else {
+  /*
+   * running from test_runner.html;
+   * only need to load requirator.js
+   */
 
-  it('should load a module with no dependencies provided', function(done) {
-    require(['modules/noDependencies.js'], function(module) {
-      expect(module).to.be.ok;
-      done();
+   // ugh
+   document.write('<script src="../lib/requirator.js"></script>');
+}
+
+(function() {
+
+  var expect = chai.expect;
+
+  describe('requirator', function() {
+
+    it('should load a module with no dependencies provided', function(done) {
+      require(['modules/noDependencies.js'], function(module) {
+        expect(module).to.be.ok;
+        done();
+      });
     });
-  });
 
-  it('should work when A requires B', function(done) {
-    require(['modules/a.js'], function(a) {
-      expect(a).to.equal('success');
-      done();
+    it('should work when A requires B', function(done) {
+      require(['modules/a.js'], function(a) {
+        expect(a).to.equal('success');
+        done();
+      });
     });
-  });
 
-});
+  });
+})();
